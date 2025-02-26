9886972da557075d079087ecbca9aef69f24a08a
Adam Cherek <111668308+adbreeker@users.noreply.github.com>
2025-02-26 21:49:18 +0100

    adding 4 latest gaps sequences
    tokuda, ciura, lee, and s. e. j.

--- ShellsortResearch.cpp
+++ ShellsortResearch.cpp
@@ -3,14 +3,15 @@
 #include <algorithm>
 #include <chrono>
 #include <thread>
+#include <math.h>
+#include <future>
 
 struct Result
 {
     double time;
     std::string name;
+    std::vector<int> gapsUsed;
 };
-
-std::vector<Result> results;
 
 void shellSort(std::vector<int>& arr, std::vector<int>& gaps)
 {
@@ -29,56 +30,167 @@ void shellSort(std::vector<int>& arr, std::vector<int>& gaps)
     }
 }
 
-void measureSortTime(void(*sortFunc)(std::vector<int>&), std::vector<int> data, const std::string& name) {
+void measureSortTime(void(*sortFunc)(std::vector<int>&), std::vector<int> data, std::string name, std::promise<Result> output) 
+{
     auto start = std::chrono::high_resolution_clock::now();
     sortFunc(data);
     auto stop = std::chrono::high_resolution_clock::now();
 
     std::chrono::duration<double, std::milli> elapsed = stop - start;
-    results.push_back(Result{ elapsed.count(), name });
+    output.set_value(Result{ elapsed.count(), name });
 }
 
-void measureShellSortTime(std::vector<int> data, std::vector<int> gaps) {
+void measureShellSortTime(std::vector<int> data, std::vector<int> gaps, std::string name, std::promise<Result> output) 
+{
     auto start = std::chrono::high_resolution_clock::now();
     shellSort(data, gaps);
     auto stop = std::chrono::high_resolution_clock::now();
 
     std::chrono::duration<double, std::milli> elapsed = stop - start;
-    results.push_back(Result{ elapsed.count(), "Shell Sort" });
+    output.set_value(Result{ elapsed.count(), name, gaps });
 }
 
-int main() {
-    const int SIZE = 10000;
-    
-    for (int i = 0; i < 100; i++)
+std::vector<int> getTokudaGaps(int sortingRange)
+{
+    std::vector<int> tokudaGaps;
+
+    for (int k = 1; k < sortingRange; k++)
     {
-        results = std::vector<Result>();
-        std::vector<int> data(SIZE);
+        int gap = (int)std::ceil((std::pow(9.0 / 4.0, k) - 1) / ((9.0 / 4.0) - 1));
+
+        if (gap < sortingRange)
+        {
+            tokudaGaps.push_back(gap);
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    std::reverse(tokudaGaps.begin(), tokudaGaps.end());
+    return tokudaGaps;
+}
+
+std::vector<int> getCiuraGaps(int sortingRange)
+{
+    std::vector<int> ciuraGaps;
+    for (int gap : {1, 4, 10, 23, 57, 132, 301, 701 /*later extension:*/, 1750})
+    {
+        if (gap < sortingRange)
+        {
+            ciuraGaps.push_back(gap);
+        }
+        else break;
+    }
+
+    std::reverse(ciuraGaps.begin(), ciuraGaps.end());
+    return ciuraGaps;
+}
+
+std::vector<int> getLeeGaps(int sortingRange)
+{
+    std::vector<int> leeGaps;
+
+    double lambda = 2.24360906142;
+
+    for (int k = 1; k < sortingRange; k++)
+    {
+        int gap = (int)std::ceil((std::pow(lambda, k) - 1) / (lambda - 1));
+
+        if (gap < sortingRange)
+        {
+            leeGaps.push_back(gap);
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    std::reverse(leeGaps.begin(), leeGaps.end());
+    return leeGaps;
+}
+
+std::vector<int> getSkeanEhrenborgJaromczykGaps(int sortingRange)
+{
+    std::vector<int> sejGaps;
+
+    for (int k = -1; k < sortingRange; k++)
+    {
+        int gap = (int)std::floor(4.0816 * std::pow(8.5714, k / 2.2449));
+
+        if (gap < sortingRange)
+        {
+            sejGaps.push_back(gap);
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    std::reverse(sejGaps.begin(), sejGaps.end());
+    return sejGaps;
+}
+
+int main() 
+{
+    const int SORTING_RANGE = 10000;
+
+    std::vector<int> tokudaGaps = getTokudaGaps(SORTING_RANGE);
+    std::vector<int> ciuraGaps = getCiuraGaps(SORTING_RANGE);
+    std::vector<int> leeGaps = getLeeGaps(SORTING_RANGE);
+    std::vector<int> sejGaps = getSkeanEhrenborgJaromczykGaps(SORTING_RANGE);
+
+    int winners[4] = { 0, 0, 0, 0 };
+
+    for (int i = 0; i < 1000; i++)
+    {
+        std::cout << "Iteration " << i << "\n";
+
+        std::vector<Result> results;
+        std::vector<int> data(SORTING_RANGE);
         for (int& num : data) num = rand() % 10000;
 
-        std::vector<int> data1 = data;
-        std::vector<int> data2 = data;
+        // Use promises & futures for safe thread return values
+        std::promise<Result> p1, p2, p3, p4;
+        std::future<Result> f1 = p1.get_future();
+        std::future<Result> f2 = p2.get_future();
+        std::future<Result> f3 = p3.get_future();
+        std::future<Result> f4 = p4.get_future();
 
-        std::vector<int> gaps = { 701, 301, 132, 57, 23, 10, 4, 1 };
-
-        std::thread t1(measureSortTime, [](std::vector<int>& d) { std::sort(d.begin(), d.end()); }, data1, "std::sort");
-        std::thread t2(measureShellSortTime, data2, gaps);
+        std::thread t1(measureShellSortTime, data, tokudaGaps, "Tokuda", std::move(p1));
+        std::thread t2(measureShellSortTime, data, ciuraGaps, "Ciura", std::move(p2));
+        std::thread t3(measureShellSortTime, data, leeGaps, "Lee", std::move(p3));
+        std::thread t4(measureShellSortTime, data, sejGaps, "SEJ", std::move(p4));
 
         t1.join();
         t2.join();
+        t3.join();
+        t4.join();
 
+        // Get results from threads
+        results.push_back(f1.get());
+        results.push_back(f2.get());
+        results.push_back(f3.get());
+        results.push_back(f4.get());
+
+        // Sort results to find the fastest
         std::sort(results.begin(), results.end(), [](const Result& a, const Result& b) {
             return a.time < b.time;
             });
 
-        std::cout << "\n\n   Iteration " << i;
-        if (results[0].name == "Shell Sort") { std::cout << " ---------------- SHELL WINNER !!! ------------------"; }
-        std::cout << "\n";
-        for (int j = 0; j < results.size(); j++)
-        {
-            std::cout << results[j].name << ": " << results[j].time << " ms\n";
-        }
+        if (results[0].name == "Tokuda") { winners[0]++; }
+        if (results[0].name == "Ciura") { winners[1]++; }
+        if (results[0].name == "Lee") { winners[2]++; }
+        if (results[0].name == "SEJ") { winners[3]++; }
     }
+
+    std::cout << "Tokuda: " << winners[0] << "\n";
+    std::cout << "Ciura: " << winners[1] << "\n";
+    std::cout << "Lee: " << winners[2] << "\n";
+    std::cout << "SEJ: " << winners[3] << "\n";
 
     return 0;
 }
