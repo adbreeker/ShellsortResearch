a71fb7eb7107b0fbe5c2fa86b8cde353594e3c40
Adam Cherek <111668308+adbreeker@users.noreply.github.com>
2025-02-27 16:54:52 +0100

    functions for comparing shell sorts

--- ShellsortResearch.cpp
+++ ShellsortResearch.cpp
@@ -13,6 +13,7 @@ struct Result
     double time;
     std::string name;
     std::vector<long> gapsUsed;
+    int wins = 0;
 };
 
 void shellSort(std::vector<int>& arr, std::vector<long>& gaps)
@@ -188,75 +189,168 @@ std::vector<long> getRandomizedGaps(long sortingRange)
     return randomizedGaps;
 }
 
+std::vector<Result> compareShellSorts(long sortingRange, std::vector<std::vector<long>> gapSequences, int iterations)
+{
+    int sortsCount = gapSequences.size();
+    std::vector<Result> avgResults(sortsCount);
+
+    std::cout << "Compare iterations:";
+
+    for (int i = 0; i < iterations; i++)
+    {
+        if (i % 50 == 0) std::cout << "\n";
+        std::cout << "+";
+
+        std::vector<int> data(sortingRange);
+        for (int& num : data) num = rand() % 10000;
+
+        // Use promises & futures for safe thread return value
+        std::vector<std::promise<Result>> promises(sortsCount);
+        std::vector < std::future<Result>> futures(sortsCount);
+        for (int j = 0; j < sortsCount; j++) futures[j] = promises[j].get_future();
+
+        std::vector<std::thread> threads;
+       
+        for (int j = 0; j < sortsCount; j++) threads.push_back(std::thread(measureShellSortTime, data, gapSequences[j], "Random" + std::to_string(j), std::move(promises[j])));
+
+        for (int j = 0; j < sortsCount; j++) threads[j].join();
+
+        // Get results from threads
+        std::vector<Result> results;
+        for (int j = 0; j < sortsCount; j++) results.push_back(futures[j].get());
+
+        if (i == 0)
+        {
+            for (int j = 0; j < sortsCount; j++)
+            {
+                avgResults[j] = results[j];
+            }
+        }
+        else
+        {
+            for (int j = 0; j < sortsCount; j++)
+            {
+                avgResults[j].time += results[j].time;
+            }
+        }
+
+        std::sort(results.begin(), results.end(), [](const Result& a, const Result& b) {
+            return a.time < b.time;
+            });
+
+        for (Result& r : avgResults) if (r.name == results[0].name) { r.wins++; }
+    }
+
+    for (Result& r : avgResults) r.time = r.time / iterations;
+
+    // Sort results to find the fastest
+    std::sort(avgResults.begin(), avgResults.end(), [](const Result& a, const Result& b) {
+        return a.time < b.time;
+        });
+
+    return avgResults;
+}
+
+std::vector<Result> compareShellWith_stdSort_Ciura_SEJ(long sortingRange, std::vector<long> gapSequence, int iterations)
+{
+    std::vector<Result> avgResults(4);
+
+    std::vector<long> gapsCiura = getCiuraGaps(sortingRange);
+    std::vector<long> gapsSEJ = getSkeanEhrenborgJaromczykGaps(sortingRange);
+
+    std::cout << "Compare iterations:";
+
+    for (int i = 0; i < iterations; i++)
+    {
+        if (i % 50 == 0) std::cout << "\n";
+        std::cout << "+";
+
+        std::vector<int> data(sortingRange);
+        for (int& num : data) num = rand() % 10000;
+
+        // Use promises & futures for safe thread return value
+        std::promise<Result> pCompare, pStdSort, pCiura, pSEJ;
+        std::future<Result> fCompare = pCompare.get_future();
+        std::future<Result> fStdSort = pStdSort.get_future();
+        std::future<Result> fCiura = pCiura.get_future();
+        std::future<Result> fSEJ = pSEJ.get_future();
+
+        std::vector<std::thread> threads;
+
+        std::thread tCompare(measureShellSortTime, data, gapSequence, "Compared", std::move(pCompare));
+        std::thread tStdSort(measureSortTime, [](std::vector<int>& d) {std::sort(d.begin(), d.end());}, data, "stdSort", std::move(pStdSort));
+        std::thread tCiura(measureShellSortTime, data, gapsCiura, "Ciura", std::move(pCiura));
+        std::thread tSEJ(measureShellSortTime, data, gapsSEJ, "SEJ", std::move(pSEJ));
+
+        tCompare.join();
+        tStdSort.join();
+        tCiura.join();
+        tSEJ.join();
+
+        std::vector<Result> results;
+        results.push_back(fCompare.get());
+        results.push_back(fStdSort.get());
+        results.push_back(fCiura.get());
+        results.push_back(fSEJ.get());
+
+        // Get results from threads
+        if (i == 0)
+        {
+            avgResults[0] = results[0];
+            avgResults[1] = results[1];
+            avgResults[2] = results[2];
+            avgResults[3] = results[3];
+        }
+        else
+        {
+            avgResults[0].time += results[0].time;
+            avgResults[1].time += results[1].time;
+            avgResults[2].time += results[2].time;
+            avgResults[3].time += results[3].time;
+        }
+
+        std::sort(results.begin(), results.end(), [](const Result& a, const Result& b) {
+            return a.time < b.time;
+            });
+
+        for (Result& r : avgResults) if (r.name == results[0].name) { r.wins++; }
+    }
+
+    for (Result& r : avgResults) r.time = r.time / iterations;
+
+    // Sort results to find the fastest
+    std::sort(avgResults.begin(), avgResults.end(), [](const Result& a, const Result& b) {
+        return a.time < b.time;
+        });
+
+    return avgResults;
+}
+
 int main() 
 {
-    const long SORTING_RANGE = 1000;
+    const long SORTING_RANGE = 5000;
 
     std::vector<long> tokudaGaps = getTokudaGaps(SORTING_RANGE);
     std::vector<long> ciuraGaps = getCiuraGaps(SORTING_RANGE);
     std::vector<long> leeGaps = getLeeGaps(SORTING_RANGE);
     std::vector<long> sejGaps = getSkeanEhrenborgJaromczykGaps(SORTING_RANGE);
 
-    std::vector<long> randomizedGaps[95];
-    for (std::vector<long>& rg : randomizedGaps) rg = getRandomizedGaps(SORTING_RANGE);
+    std::vector<std::vector<long>> randomizedGaps = { tokudaGaps, ciuraGaps, leeGaps, sejGaps};
+    for (int i = 0; i<96; i++) randomizedGaps.push_back(getRandomizedGaps(SORTING_RANGE));
 
-    for (std::vector<long> rg : randomizedGaps)
+    std::vector<Result> results = compareShellSorts(SORTING_RANGE ,randomizedGaps, 100);
+
+    /*std::vector<Result> results = compareShellWith_stdSort_Ciura_SEJ(
+        SORTING_RANGE,
+        std::vector<long> {1},
+        1000);*/
+
+    std::cout << "\n\n    Results:\n";
+
+    for (Result r : results)
     {
-        for (long gap : rg)
-        {
-            std::cout << gap << "  ";
-        }
-        std::cout << "\n";
+        std::cout << r.name << ": " << r.time << "ms | " << r.wins << "w\nGaps: ";
+        for (long gap : r.gapsUsed) std::cout << gap << " ";
+        std::cout << "\n\n";
     }
-
-    int winners[6] = {};
-    
-    std::cout << "\n\n";
-
-    for (int i = 0; i < 1; i++)
-    {
-        std::cout << "Iteration " << i << "\n";
-
-        std::vector<Result> results;
-        std::vector<int> data(SORTING_RANGE);
-        for (int& num : data) num = rand() % 10000;
-
-        // Use promises & futures for safe thread return value
-        std::promise<Result> promises[100];
-        std::future<Result> futures[100];
-        for (int j = 0; j < 100; j++) futures[j] = promises[j].get_future();
-
-        std::thread threads[100];
-
-        threads[0] = std::thread(measureShellSortTime, data, tokudaGaps, "Tokuda", std::move(promises[0]));
-        threads[1] = std::thread(measureShellSortTime, data, ciuraGaps, "Ciura", std::move(promises[1]));
-        threads[2] = std::thread(measureShellSortTime, data, leeGaps, "Lee", std::move(promises[2]));
-        threads[3] = std::thread(measureShellSortTime, data, sejGaps, "SEJ", std::move(promises[3]));
-        threads[4] = std::thread(measureSortTime, [](std::vector<int>& d) {std::sort(d.begin(), d.end());}, data, "stdSort", std::move(promises[4]));
-
-        for (int j = 5; j<100; j++) threads[j] = std::thread(measureShellSortTime, data, randomizedGaps[j-5], "Random" + std::to_string(j-4), std::move(promises[j]));
-
-        for (int j = 0; j < 100; j++) threads[j].join();
-
-        // Get results from threads
-        for (int j = 0; j < 100; j++) results.push_back(futures[j].get());
-
-        // Sort results to find the fastest
-        std::sort(results.begin(), results.end(), [](const Result& a, const Result& b) {
-            return a.time < b.time;
-            });
-
-        for (Result r : results)
-        {
-            std::cout << r.name + ": " << r.time << "ms\n";
-            if (r.name != "stdSort")
-            {
-                std::cout << "Gaps: ";
-                for (long gap : r.gapsUsed) std::cout << gap << " ";
-            }
-            std::cout << "\n\n";
-        }
-    }
-
-    return 0;
 }
