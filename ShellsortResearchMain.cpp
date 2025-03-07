be39b22ceb98f89830faff4dfa72c71f871c889e
Adam Cherek <111668308+adbreeker@users.noreply.github.com>
2025-03-07 13:43:46 +0100

    printing results function

--- ShellsortResearchMain.cpp
+++ ShellsortResearchMain.cpp
@@ -2,31 +2,36 @@
 #include <vector>
 #include "GeneticAlgorithm.hpp"
 #include "Shellsort.hpp"
+#include "ShellsortComparisions.hpp"
 
+const unsigned long SORTING_RANGE = 1000;
 
-
-unsigned long SORTING_RANGE = 1000;
-
-int main() 
+void PrintResults(std::vector<Result>& results)
 {
-    std::vector<Result> results;
-
-    GapsSequence tokudaGaps = getTokudaGaps(SORTING_RANGE);
-    GapsSequence ciuraGaps = getCiuraGaps(SORTING_RANGE);
-    GapsSequence leeGaps = getLeeGaps(SORTING_RANGE);
-    GapsSequence sejGaps = getSkeanEhrenborgJaromczykGaps(SORTING_RANGE);
-
-    std::vector<GapsSequence> randomizedGapsSequences = { tokudaGaps, ciuraGaps, leeGaps, sejGaps};
-    for (int i = randomizedGapsSequences.size(); i<100; i++) randomizedGapsSequences.push_back(GapsSequence("Random" + std::to_string(i), getRandomizedGaps(SORTING_RANGE)));
-
-    genetic::endlessGapsSeeking(SORTING_RANGE, randomizedGapsSequences, 100);
-
-    /*results = geneticAlgorithmForGapsSeeking(SORTING_RANGE, randomizedGapsSequences, 50, 100);
-
     std::cout << "\n\nResults:\n";
-    for (Result r : results)
+    for (Result& r : results)
     {
         r.gapsSequence.PrintInstance();
         std::cout << "\n  Time: " << r.time << "ms | Operations: " << r.operations << " | Wins: " << r.wins << "\n\n";
-    }*/
+    }
+}
+
+int main() 
+{
+    std::vector<GapsSequence> gapsSequences = 
+    { 
+        getTokudaGaps(SORTING_RANGE),
+        getCiuraGaps(SORTING_RANGE),
+        getLeeGaps(SORTING_RANGE),
+        getSkeanEhrenborgJaromczykGaps(SORTING_RANGE),
+        GapsSequence("Compare1", {989, 269, 118, 41, 20, 9, 4, 1 }),
+        GapsSequence("Compare2", {895, 283, 91, 29, 14, 4, 1}),
+    };
+
+
+    //for (int i = gapsSequences.size(); i<100; i++) gapsSequences.push_back(GapsSequence("Random" + std::to_string(i), getRandomizedGaps(SORTING_RANGE)));
+    //genetic::endlessGapsSeeking(SORTING_RANGE, gapsSequences, 100);
+
+    auto results = compareShellSorts(SORTING_RANGE, gapsSequences, 1);
+    PrintResults(results);
 }
